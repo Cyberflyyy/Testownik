357ebcc6ec8b3d8b344440e0fefdd98ef8953c43
unknown <bkarpinski798@wp.pl>
2024-10-31 18:45:53 +0100

    Opis zmian

--- src/app/home/1/page.tsx
+++ src/app/home/1/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import React from 'react'
 import Header_main from '@/components/header_main'
-import Body_main from '@/components/Body_main'
+import Body_main from '@/components/body_main'
 const page = () => {
     const [isTestStarted, setIsTestStarted] = useState(false)
     const handleReturnToStart = () => {
