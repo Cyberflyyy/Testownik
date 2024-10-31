92acc1608e2190acc1d23850e8cf5068705ae8b3
unknown <bkarpinski798@wp.pl>
2024-10-31 18:58:41 +0100

    Opis zmian

--- src/components/SessionProvider.tsx
+++ src/components/SessionProvider.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import { SessionProvider as Provider } from "next-auth/react"
+import { Session } from "next-auth"
 
 type Props = {
   children: React.ReactNode
-  session: any
+  session: Session | null // Użyj odpowiedniego typu
 }
 
 export default function SessionProvider({ children, session }: Props) {
